1bf9ef145f2313c6c9f1e4f5da0cd766809e0738
Diego Del Real <ddelreal760@gmail.com>
2024-10-03 20:24:17 +0000

    fix and remove branch - most up to date code

--- Main.java
+++ Main.java
@@ -4,70 +4,35 @@
 
 public class Main
 {
-  //ascii art credit: http://www.oocities.org/spunk1111/bodypart.htm
-  final static String ASCII_ART_0 = "    .----.    .----.  ",
-  ASCII_ART_1 = "   (  --  \\  /  --  )",
-  ASCII_ART_2 = "          |  |        ",
-  ASCII_ART_3 = "         _/  \\_      ",
-  ASCII_ART_4 = "        (_    _)      ",
-  ASCII_ART_5 = "     ,    `--`    ,   ",
-  ASCII_ART_6 = "     \\'-.______.-'/  ",
-  ASCII_ART_7 = "      \\          /   ",
-  ASCII_ART_8 = "       '.--..--.'     ",
-  ASCII_ART_9 = "         `\"\"\"\"\"` ",
-  ASCII_CREDIT = "   ascii art by: jgs    ";
+        final static String ASCII_ART_0 = "    .----.    .----.  ",
+                ASCII_ART_1 = "   (  --  \\  /  --  )",
+                ASCII_ART_2 = "          |  |        ",
+                ASCII_ART_3 = "         _/  \\_      ",
+                ASCII_ART_4 = "        (_    _)      ",
+                ASCII_ART_5 = "     ,    `--`    ,   ",
+                ASCII_ART_6 = "     \\'-.______.-'/  ",
+                ASCII_ART_7 = "      \\          /   ",
+                ASCII_ART_8 = "       '.--..--.'     ",
+                ASCII_ART_9 = "         `\"\"\"\"\"` ",
+                ASCII_CREDIT = "   ascii art by: jgs    ";
 
-final static String TITLE_USA = "UNITED STATES OF AMERICA",
-  TITLE_EAC = "EMPLOYMENT AUTHORIZATION CARD";
-final static String LABEL_SURNAME = "Surname", LABEL_GIVEN_NAME = "Given Name", LABEL_USCIS_NUM = "USCIS#",
-  LABEL_CATEGORY = "Category", LABEL_CARD_NUM = "Card#", LABEL_BIRTH_COUNTRY = "Country of Birth",
-  LABEL_TERMS_CONDITIONS = "Terms and Conditions", LABEL_BIRTH_DATE = "Date of Birth",
-  LABEL_SEX = "Sex", LABEL_VALID_DATE = "Valid From:", LABEL_EXPIRE_DATE = "Card Expires:",
-  LABEL_REENTRY_DISCLAIMER = "NOT VALID FOR REENTRY TO U.S.";
-
-
-
-   public static String surname, givenName, category, cardNum, birthCountry, termsAndConditions;
-        char sex;
-
-public static String formatCard(String surname, String givenName,
-String category, String cardNum, String birthCountry, 
-String termsAndConditions, char sex, String uscisNum, 
-String dateOfBirth, String validDate, String expireDate)
-{
-        String fullCard = "";
-        fullCard += String.format("╔══════════════════════════════════════════════════════════════════════╗%n");
-        fullCard += String.format("║%35s%35s║%n", TITLE_USA, "");
-        fullCard += String.format("║%60s%10s║%n", TITLE_EAC, "");
-        fullCard += String.format("║%-25s%-45s║%n", "", LABEL_SURNAME);
-        fullCard += String.format("║%-25s%-45s║%n", "", surname);
-        fullCard += String.format("║%-25s%-45s║%n", ASCII_ART_0, LABEL_GIVEN_NAME);
-        fullCard += String.format("║%-25s%-45s║%n", ASCII_ART_1, givenName);
-        fullCard += String.format("║%-25s%-15s%-15s%-15s║%n", ASCII_ART_2, LABEL_USCIS_NUM, LABEL_CATEGORY, LABEL_CARD_NUM);
-        fullCard += String.format("║%-25s%-15s%-15s%-15s║%n", ASCII_ART_3, uscisNum, category, cardNum);
-        fullCard += String.format("║%-25s%-45s║%n", ASCII_ART_4, LABEL_BIRTH_COUNTRY);
-        fullCard += String.format("║%-25s%-45s║%n", ASCII_ART_5, birthCountry);
-        fullCard += String.format("║%-25s%-45s║%n", ASCII_ART_6, LABEL_TERMS_CONDITIONS);
-        fullCard += String.format("║%-25s%-45s║%n", ASCII_ART_7, termsAndConditions);
-        fullCard += String.format("║%-25s%-15s%-30s║%n", ASCII_ART_8, LABEL_BIRTH_DATE, LABEL_SEX);
-        fullCard += String.format("║%-25s%-15s%-30s║%n", ASCII_ART_9, dateOfBirth, sex);
-        fullCard += String.format("║%-25s%-15s%-30s║%n", "", LABEL_VALID_DATE, validDate);
-        fullCard += String.format("║%-25s%-15s%-30s║%n", "", LABEL_EXPIRE_DATE, expireDate);
-        fullCard += String.format("║%-25s%-45s║%n", ASCII_CREDIT, LABEL_REENTRY_DISCLAIMER);
-        fullCard += String.format("╚══════════════════════════════════════════════════════════════════════╝%n");
-        return fullCard;
-}
-
+        final static String TITLE_USA = "UNITED STATES OF AMERICA",
+                TITLE_EAC = "EMPLOYMENT AUTHORIZATION CARD";
+        final static String LABEL_SURNAME = "Surname", LABEL_GIVEN_NAME = "Given Name", LABEL_USCIS_NUM = "USCIS#",
+                LABEL_CATEGORY = "Category", LABEL_CARD_NUM = "Card#", LABEL_BIRTH_COUNTRY = "Country of Birth",
+                LABEL_TERMS_CONDITIONS = "Terms and Conditions", LABEL_BIRTH_DATE = "Date of Birth",
+                LABEL_SEX = "Sex", LABEL_VALID_DATE = "Valid From:", LABEL_EXPIRE_DATE = "Card Expires:",
+                LABEL_REENTRY_DISCLAIMER = "NOT VALID FOR REENTRY TO U.S.";
     public static void main(String[] args)
     {
-      Main.formatCard("CHAPETON-LAMAS", );
-    }
+        //ascii art credit: http://www.oocities.org/spunk1111/bodypart.htm
+        
 
-
-    
         //DECLARATION SECTION
         //complete fields
-        
+        String surname, givenName, category, cardNum, birthCountry, termsAndConditions;
+        char sex;
+
         //parts of fields
         String birthMonth;
         int uscisNum1, uscisNum2, uscisNum3, birthDay, birthYear,
@@ -103,17 +68,53 @@ String dateOfBirth, String validDate, String expireDate)
 
         //Strings to help clean up long printf's below
         uscisNum = String.format("%03d-%03d-%03d", uscisNum1, uscisNum2, uscisNum3);
-        dateOfBirth = String.format("%02d %s %d", birthDay, birthMonth, birthYear);
-        validDate = String.format("%02d/%02d/%4d", validMonth, validDay, validYear);
-        expireDate = String.format("%02d/%02d/%4d", expireMonth, expireDay, expireYear);
+        dateOfBirth = String.format("%02d %s %d", birthDay, birthMonth, birthYear);     
+        validDate = Main.calcDate(2, 2, 2020);
+        expireDate = Main.calcDate(2, 2, 2022);
 
-        
+        System.out.println(Main.formatCard(surname, givenName, category, cardNum, birthCountry, termsAndConditions, sex, uscisNum, dateOfBirth, validDate, expireDate));
+    }
 
 
         //INPUT + CALCULATION SECTION
         //N/A
 
         //OUTPUT SECTION
-        
+        public static String formatCard(String surname, String givenName,
+    String category, String cardNum, String birthCountry, 
+    String termsAndConditions, char sex, String uscisNum, 
+    String dateOfBirth, String validDate, String expireDate)
+    {
+        String fullcard = "";
+        fullcard += String.format("╔══════════════════════════════════════════════════════════════════════╗%n");
+        fullcard += String.format("║%35s%35s║%n", TITLE_USA, "");
+        fullcard += String.format("║%60s%10s║%n", TITLE_EAC, "");
+        fullcard += String.format("║%-25s%-45s║%n", "", LABEL_SURNAME);
+        fullcard += String.format("║%-25s%-45s║%n", "", surname);
+        fullcard += String.format("║%-25s%-45s║%n", ASCII_ART_0, LABEL_GIVEN_NAME);
+        fullcard += String.format("║%-25s%-45s║%n", ASCII_ART_1, givenName);
+        fullcard += String.format("║%-25s%-15s%-15s%-15s║%n", ASCII_ART_2, LABEL_USCIS_NUM, LABEL_CATEGORY, LABEL_CARD_NUM);
+        fullcard += String.format("║%-25s%-15s%-15s%-15s║%n", ASCII_ART_3, uscisNum, category, cardNum);
+        fullcard += String.format("║%-25s%-45s║%n", ASCII_ART_4, LABEL_BIRTH_COUNTRY);
+        fullcard += String.format("║%-25s%-45s║%n", ASCII_ART_5, birthCountry);
+        fullcard += String.format("║%-25s%-45s║%n", ASCII_ART_6, LABEL_TERMS_CONDITIONS);
+        fullcard += String.format("║%-25s%-45s║%n", ASCII_ART_7, termsAndConditions);
+        fullcard += String.format("║%-25s%-15s%-30s║%n", ASCII_ART_8, LABEL_BIRTH_DATE, LABEL_SEX);
+        fullcard += String.format("║%-25s%-15s%-30s║%n", ASCII_ART_9, dateOfBirth, sex);
+        fullcard += String.format("║%-25s%-15s%-30s║%n", "", LABEL_VALID_DATE, validDate);
+        fullcard += String.format("║%-25s%-15s%-30s║%n", "", LABEL_EXPIRE_DATE, expireDate);
+        fullcard += String.format("║%-25s%-45s║%n", ASCII_CREDIT, LABEL_REENTRY_DISCLAIMER);
+        fullcard += String.format("╚══════════════════════════════════════════════════════════════════════╝%n");
+        return fullcard;
     }
+    public static String calcDate(int month, int Day, int Year)
+    {
+        String formattedDate;
+        formattedDate = String.format("%02d/%02d/%4d", month, Day, Year);
+
+        return formattedDate;
+
+    }
+
+
 }
